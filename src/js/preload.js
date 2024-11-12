11275dbe55e6fcbddfcbe024e53873ee42faef63
Alevir Coelho Neto <alevirramos@gmail.com>
2024-11-12 16:01:38 -0300

    Reduced image size for faster loading and adjusted button animation

--- src/js/preload.js
+++ src/js/preload.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function () {
     const preloader = document.getElementById("preloader");
     setTimeout(function () {
       preloader.style.display = "none";
-    }, 1000); // 2000ms = 2 segundos de atraso
+    }, 800); //
   });
